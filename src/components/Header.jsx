d8ee15cc6beea64965449a33229ad2313582c85b
Camilo318 <camiloa0318@gmail.com>
2021-01-05 12:58:25 -0500

    Add responsiveness to Navbar Add Toogle and Collapse Add expand break point

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,22 @@ import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap"
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar bg="dark" expand="md" variant="dark">
         <Container>
           <Navbar.Brand href="/">Blohsh Store</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            <Nav.Link href="/sign-in">Sign In</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart mr-2"></i>
+                <span>Cart</span>
+              </Nav.Link>
+              <Nav.Link href="/sign-in">
+                <i className="fas fa-user mr-2"></i>
+                <span>Sign In</span>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
